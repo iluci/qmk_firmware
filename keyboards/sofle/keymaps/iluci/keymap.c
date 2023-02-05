78fd84a8ef49ee9d448bf0f8a0173d7d644d52ab
luciangi <luciangabrielilie@gmail.com>
2023-02-05 16:53:19 +0000

    sofle layer update

--- keyboards/sofle/keymaps/iluci/keymap.c
+++ keyboards/sofle/keymaps/iluci/keymap.c
@@ -495,12 +495,13 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_270;
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
 }
 
 static void print_primary(void) {
-    oled_write_P(PSTR("Sofle"), false);
-    oled_write_P(PSTR("\n\n"), false);
     switch (get_highest_layer(default_layer_state)) {
         case _QWRTY:
             oled_write_P(PSTR("QWRTY"), false);
@@ -512,17 +513,12 @@ static void print_primary(void) {
             oled_write_P(PSTR("UNDEF"), false);
     }
     oled_write_P(PSTR("\n\n"), false);
-    oled_write_P(PSTR("OS   "), false);
-    switch (os_mode) {
-        case OS_MODE_WIN:
-            oled_write_P(PSTR("Win  "), false);
-            break;
-        case OS_MODE_MAC:
-            oled_write_P(PSTR("Mac  "), false);
-            break;
-        default:
-            oled_write_P(PSTR("Def  "), false);
-    }
+    led_t led_usb_state = host_keyboard_led_state();
+    oled_write_P(PSTR("CPSLK"), led_usb_state.caps_lock);
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("Def  "), os_mode == OS_MODE_DFT);
+    oled_write_P(PSTR("Win  "), os_mode == OS_MODE_WIN);
+    oled_write_P(PSTR("Mac  "), os_mode == OS_MODE_MAC);
     oled_write_P(PSTR("\n\n"), false);
     oled_write_P(PSTR("Layer"), false);
     switch (get_highest_layer(layer_state)) {
@@ -545,12 +541,21 @@ static void print_primary(void) {
         default:
             oled_write_P(PSTR("UNDEF"), false);
     }
-    oled_write_P(PSTR("\n\n"), false);
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 }
 
-static void print_secondary(void) {}
+static void print_secondary(void) {
+    // clang-format off
+
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    // clang-format on
+
+    oled_write_P(qmk_logo, false);
+}
 
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
